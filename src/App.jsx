88798528e23eafe88f21eb4082ce108cd23033fd
Nikhilesh Mourya <nikhleshmourya386@gmail.com>
2026-03-12 15:19:25 +0530

    adding content on the frontend

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import Chatbot from "./components/Chatbot.jsx";
+import KimiAI from "./components/Chatbot.jsx";
+
+function App() {
+  return (
+    <div>
+      {/* <Chatbot /> */}
+      <KimiAI/>
+    </div>
+  );
+}
+
+export default App;
